473b9c1ffbb592817031b591e3e37806e42e00bd
Gulshanstar <gulshanmundri64@gmail.com>
2025-03-12 13:54:12 +0530

    updated logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,33 +1,29 @@
-'''
-1. what is the error due to get exception?
-2. in which line number
-3. in which file
-'''
 import sys
 import logging
-def get_exception_details(error, error_details:sys):
-    _,_,exc_tb = error_details.exc_info()
+from src.logger import logging
+
+def get_exception_details(error_message, error_details: sys):
+    _, _, exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
-    error = error
-    error_message = 'Error occured in python script [{0}], line number [{1}] and error message [{2}]'.format(
-        file_name, line_number, error
+    error_message_format = "Error occurred in python script [{0}], line number [{1}], error message [{2}]".format(
+        file_name, line_number, str(error_message)
     )
-
-    return error_message
+    return error_message_format
 
 class CustomException(Exception):
-    def __init__(self,error,error_details:sys):
-        super().__init__(error)
-        self.error = get_exception_details(error=error, error_details=error_details)
+    def __init__(self, error_message, error_details):
+        error_details_formatted = get_exception_details(error_message, error_details)
+        super().__init__(error_details_formatted)
+        self.error_message = error_details_formatted
 
     def __str__(self):
-        return self.error
-    
-    
+        return self.error_message
 
-
-
-    
-
+if __name__ == '__main__':
+    try:
+        a = 1 / 0  # Intentional division by zero
+    except Exception as e:
+        logging.info('Exception divided by zero') # Logs traceback
+        raise CustomException(e, sys)
 
--- src/logger.py
+++ src/logger.py
@@ -1,31 +1,18 @@
-"""
-During execution logging the details 
-"""
-
 import logging
-import os
 from datetime import datetime
-
-# log_file_name = f"{datetime.now().strftime('%m/%d/%y,%H:%M:%S')}.log"
-# log_dir = os.path.join(os.getcwd(),'logs')
-
-# os.makedirs('logs',exist_ok=True)
-# file_path = os.path.join(log_dir,log_file_name)
-
-# Generate log file name with correct formatting
-log_file_name = f"{datetime.now().strftime('%m-%d-%y_%H-%M-%S')}.log"
-
-# Create logs directory and file path
-log_dir = os.path.join(os.getcwd(), 'logs')
-os.makedirs(log_dir, exist_ok=True)  # Ensure logs directory exists
-file_path = os.path.join(log_dir, log_file_name)
-
-print("Log File Path:", file_path)
+import os
+# this file must be in logs folder
+log_file_name = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+logs_folder_dir = os.path.join(os.getcwd(),'logs')
+print(logs_folder_dir)
+os.makedirs(logs_folder_dir,exist_ok=True)
+complete_path = os.path.join(logs_folder_dir,log_file_name)
 
 logging.basicConfig(
-    filename= file_path,
-    format="%(asctime)s - %(levelname)s - [%(name)s] - %(filename)s:%(lineno)d - %(message)s",
+    filename= complete_path,
+    format='%(asctime)s - %(levelname)s - [%(name)s] - %(filename)s:%(lineno)d - %(message)s',
     level=logging.INFO
 )
 
-
+if __name__ =='__main__':
+    logging.info('logging has started')
